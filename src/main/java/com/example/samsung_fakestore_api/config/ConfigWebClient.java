c344067f05c100aef344705c2cf86fdc904817ef
Cauesilvaa <43147371+Cauesilvaa@users.noreply.github.com>
2025-06-02 02:03:19 -0300

    cors

--- src/main/java/com/example/samsung_fakestore_api/config/ConfigWebClient.java
+++ src/main/java/com/example/samsung_fakestore_api/config/ConfigWebClient.java
@@ -4,12 +4,28 @@ import com.example.samsung_fakestore_api.client.CartClient;
 import com.example.samsung_fakestore_api.client.UserClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ConfigWebClient {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 
     @Bean
     public HttpServiceProxyFactory httpServiceProxyFactory(){
